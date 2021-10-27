c344cb7625c05e7a96790c97f63c2fbe6292b9cd
jraillard <jraillard@c2s.fr>
2021-10-27 17:46:09 +0200

    first commit

---
+++ RequestDelegates/API/GetUserByIdDelegate.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Primitives;
+using System;
+using System.Net;
+
+namespace blog.c2s.endpoints.RequestDelegates.Environment
+{
+    public class GetUserByIdDelegate
+    {
+        public static RequestDelegate Delegate => async context =>
+        {
+            var serviceProvider = context.RequestServices;
+            var logger = serviceProvider.GetService<ILogger<GetUserByIdDelegate>>();
+            try
+            {
+                // On récupère l'Id depuis la route
+                var stringValues = context.Request.RouteValues["id"].ToString();
+                context.Response.StatusCode = (int)HttpStatusCode.NoContent;
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex.Message);
+                logger.LogTrace(ex.StackTrace);
+                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                await context.Response.WriteAsync(ex.Message);
+            }
+        };
+    }
+}
