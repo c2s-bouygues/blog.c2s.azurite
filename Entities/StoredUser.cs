77dca66b2adda37bd5a51575af8c0460be458a9e
jraillard <jraillard@c2s.fr>
2021-11-05 14:07:04 +0100

    Création des entitées

---
+++ Entities/StoredUser.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Azure.Cosmos.Table;
+using Newtonsoft.Json;
+using System;
+
+namespace blog.c2s.azurite.Entities
+{
+    namespace blog.c2s.azurite.Entities
+    {
+        public class StoredUser : TableEntity
+        {
+            public const int LifeTimeDuration = 15;
+
+            public string Response { get; set; }
+            public DateTimeOffset ExpirationDate { get; set; }
+
+            public StoredUser() { }
+
+            public StoredUser(User userBase)
+            {
+                var serializeResponse = JsonConvert.SerializeObject(userBase);
+                Response = serializeResponse;
+                PartitionKey = nameof(StoredUser);
+                RowKey = userBase.Id.ToString();
+                ExpirationDate = DateTimeOffset.Now.AddMinutes(LifeTimeDuration);
+            }
+
+            public User User
+            {
+                get
+                {
+                    var deserializedResponse = JsonConvert.DeserializeObject<User>(Response);
+                    return deserializedResponse;
+                }
+            }
+        }
+    }
+}
